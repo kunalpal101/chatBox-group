03f0b50db0dc0d4557236d6eba8b7db7234c6d13
Kunal Pal <kunal17265@gmail.com>
2023-02-15 19:40:40 +0530

    improved UI

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -10,7 +10,7 @@ const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
-const u_name = prompt("What is your name?");
+const u_name = prompt("What is your name?") || "New User";
 appendMessage({ new_user: "You", user_message: "Joined" });
 socket.emit("new-user", u_name);
 
@@ -50,10 +50,14 @@ function appendMessage(message) {
   messageElement.innerHTML =
     "<div class='toast fade show my-3'>" +
     "<div class='toast-header'>" +
-    "<svg class='bd-placeholder-img rounded me-2' width='20' height='20' xmlns='http://www.w3.org/2000/svg'" +
-    "aria-hidden='true' preserveAspectRatio='xMidYMid slice' focusable='false'>" +
-    "<rect width=100% height=100% fill=#007aff></rect> </svg>" +
-    "<strong class='me-auto'>" +
+    // "<svg class='bd-placeholder-img rounded me-2' width='20' height='20' xmlns='http://www.w3.org/2000/svg'" +
+    // "aria-hidden='true' preserveAspectRatio='xMidYMid slice' focusable='false'>" +
+    // "<rect width=100% height=100% fill=#007aff></rect> </svg>" +
+    "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='me-2 bi bi-person-circle' viewBox='0 0 16 16'>" +
+    "<path d='M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z'/>" +
+    "<path fill-rule='evenodd' d='M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z'/>" +
+    "</svg>  " +
+    "<strong class='me-auto'> " +
     username +
     "</strong>" +
     "</div> <div class='toast-body'>" +
